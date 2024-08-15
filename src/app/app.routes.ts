bbd89588770c100ccda43c5da1251939361487d4
chacon <vitorlcahcon@gmail.com>
2024-08-15 17:39:15 +0100

    feat: Add lazy loading for user module and update Angular dependencies

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'user', loadChildren: () => import('./user/user.module').then(m => m.UserModule) },
+  { path: '', redirectTo: '/user', pathMatch: 'full' }
+];
