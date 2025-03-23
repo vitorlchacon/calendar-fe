f0463264b5c3a4cfd65449bdc81f7512a7038f38
chacon <vitorlchacon@gmail.com>
2025-03-23 19:53:41 +0000

    feat: env setup

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,11 +4,11 @@ export const environment = {
   defaultLanguage: 'pt-BR',
   version: '1.0.0',
   keycloak: {
-    url: 'http://keycloak:8443',
+    url: 'http://192.168.1.211:8443',
     realm: 'calendar',
     clientId: 'calendar'
   },
   backend: {
-    url: 'http://calendar-be:8080',
+    url: 'http://192.168.1.211:8080',
   }
 };
